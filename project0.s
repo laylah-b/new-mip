1a2eead6e89cc113fa6393baa1c89494a67e6f35
laylah-b <81383658+laylah-b@users.noreply.github.com>
2021-09-30 22:36:27 -0400

    Update project0.s

--- project0.s
+++ project0.s
@@ -1,16 +1,14 @@
 # PROGRAM: Hello, World!
- .data # Data declaration section
+ .data
  out_string: .asciiz "\nHello, World!\n"
- .text # Assembly language instructions
-main: # Start of code section
- li $v0, 4 # system call code for printing string = 4
- la $a0, out_string # load address of string
-# to be printed into $a0
- syscall # call operating system to perform operation in $v0
-# syscall takes its arguments from $a0, $a1, ...
- lb $t1, 2($a0) # Read 'e' (the ASCII code) from memory
- addi $t1, $t1, -32 # Change its ASCII code to 'E'
- sb $t1, 2($a0) # Write the new ASCII code to memory
- syscall # Print the string again
+ .text
+main:
+ li $v0, 4
+ la $a0, out_string
+ syscall
+ lb $t1, 2($a0)
+ addi $t1, $t1, -32
+ sb $t1, 2($a0)
+ syscall
  li $v0, 10
- syscall # Exit program
+ syscall
